0a48f011bdc9b0b151792f780f947b6515b88fd2
Roman <62894917+Wolfengo@users.noreply.github.com>
2024-06-12 21:21:15 +0300

    config add

---
+++ bot_script/config.py
@@ -0,0 +1,26 @@
+TEST = True
+
+# Токен бота
+TEST_TOKEN = ""
+REALISE_TOKEN = ""
+
+BOT_TOKEN = TEST_TOKEN if TEST is True else REALISE_TOKEN
+
+# Токен оплаты
+PAYMENTS_TOKEN = ""
+
+# Имя пользователя, пароль, хост, порт в PostgreSQL
+db_user = "postgres"
+db_password = "password"
+db_password_postgres = 'password'
+db_host = 'localhost'
+db_port = 5432
+
+# Имя базы данных
+db_name = "telegram"
+
+# time
+timeZone = +3
+
+# default localization
+localization = 'en'
